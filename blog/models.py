f333a1b47db0f5964fa776f9e576e3c39fe31f29
andbandu <andbanduln@gmail.com>
2020-03-21 17:29:57 +0530

    nxtcommt

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,5 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
     date = models.DateField()
+    def __str__(self):
+        return self.title
